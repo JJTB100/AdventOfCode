8724ffda0e18441022b3f25e38527d9f1f224c25
Jonathan Broster <jonathan@broster.co.uk>
2024-12-26 01:01:50 +0000

    I finally* finished Day 21, to complete 2024

--- 2024/Day21.py
+++ 2024/Day21.py
@@ -1,46 +1,101 @@
-with open("example.in", "r") as f:
-    lines = f.read().splitlines()
+import os
+from pathlib import Path
+from itertools import product
+from collections import Counter
 
-dir_mapper = {"A": 0, "^": 1, ">": 2, "<": 3, "v": 4}
-num_mapper = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4,
-              "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "A": 10}
-directional_map = [["", "<", "v", "<v>", "<v"],
-                   [">", "", "v>", "v<", "v"],
-                   ["^", "<^", "", "<<", "<"],
-                   [">>^", ">^", ">>", "", ">"],
-                   [">^", "^", ">", "<", ""]]
-numerical_map = [["", "^<", "^", "^>", "^^<", "^^", "^^>", "^^^<", "^^^", "^^^>", ">"],
-                 [">v", "", ">", ">>", "^", "^>",
-                     "^>>", "^^", "^^>", "^^>>", ">>v"],
-                 ["v", "<", "", ">", "<^", "^", "^>", "^^<", "^^", "^^>", ">v"],
-                 ["<v", "<<", "<", "", "^<<", "^<", "^", "^^<<", "^^<", "^^", "v"],
-                 [">vv", "v", "v>", "v>>", "", ">",
-                     ">>", "^", "^>", "^>>", ">>vv"],
-                 ["vv", "v<", "v", "v>", "<", "", ">", "<^", "^", "^>", ">vv"],
-                 ["vv<", "v<<", "v<", "v", "<<", "<", "", "^<<", "^<", "^", "vv"],
-                 [">vvv", "vv", "vv>", "vv>>", "v", "v>", "v>>", "", ">", ">>"],
-                 ["vvv", "vv<", "vv", "vv>", "v<", "v", "v>", "<", "", ">", "vvv>"],
-                 ["vvv<", "vv<<", "vv<", "vv", "v<<",
-                     "v<", "v", "<<", "<", "", "vvv"],
-                 ["<", "^<<", "^<", "^", "^^<<", "^<", "^^", "^^^<<", "^^^<", "^^^", ""]]
+DIRECTIONS = [(-1, 0), (0, 1),
+              (1, 0), (0, -1)]
+UP, RIGHT, DOWN, LEFT = DIRECTIONS
+def add(*ps): return tuple(map(sum, zip(*ps)))
 
 
-def get_direction_instr(start, end):
-    return directional_map[dir_mapper[start]][dir_mapper[end]] + "A"
+def sub(p1, p2):
+    return tuple(a-b for a, b in zip(p1, p2))
 
 
-def get_numerical_instr(start, end):
-    return numerical_map[num_mapper[start]][num_mapper[end]] + "A"
+
+with open("input.in") as file:
+    lines = file.read().splitlines()
+    
+num_pad_lines = """
+789
+456
+123
+.0A
+""".strip().splitlines()
+
+dir_pad_lines = """
+.^A
+<v>
+""".strip().splitlines()
+
+num_pad = {(i, j): c for i, line in enumerate(num_pad_lines)
+           for j, c in enumerate(line) if c != "."}
+dir_pad = {(i, j): c for i, line in enumerate(dir_pad_lines)
+           for j, c in enumerate(line) if c != "."}
+
+num_pad.update({v: k for k, v in num_pad.items()})
+dir_pad.update({v: k for k, v in dir_pad.items()})
+
+def step(source, target, pad):
+    target_i, target_j = pad[target]
+    source_i, source_j = pad[source]
+    di = target_i - source_i
+    dj = target_j - source_j
+    
+    ## Check if it goes off the path:
+    vert = "v"*di+"^"*-di
+    horiz = ">"*dj+"<"*-dj
+    if dj > 0 and (target_i, source_j) in pad:
+        return vert+horiz+"A"
+    if (source_i, target_j) in pad:
+        return horiz+vert+"A"
+    if (target_i, source_j) in pad:
+        return vert+horiz+"A"
 
 
-for line in lines:
-    line = "A"+line
-    output = ""
-    for i in range(1, len(line)):
-        output += get_numerical_instr(line[i-1], line[i])
-    print(output)
-    for i in range(1, len(output)):
-        output = output[:i] + \
-            get_direction_instr(output[i-1], output[i]) + output[i:]
+def routes(path, pad):
+    out = []
+    start = "A"
+    for end in path:
+        out.append(step(start, end, pad))
+        start = end
+    return "".join(out)
 
-    print(output)
+
+num_routes = [routes(line, num_pad) for line in lines]
+rad_routes = [routes(route, dir_pad) for route in num_routes]
+cold_routes = [routes(route, dir_pad) for route in rad_routes]
+p1 = sum(len(route) * int(line[:-1]) for route, line in zip(cold_routes, lines))
+print("Part 1:", p1)
+
+
+def routes2(path, pad):
+    out = []
+    start = "A"
+    for end in path:
+        out.append(step(start, end, pad))
+        start = end
+    return Counter(out)
+
+
+def route_len(route):
+    return sum(len(k)*v for k, v in route.items())
+
+
+robot_routes = [Counter([route]) for route in num_routes]
+for _ in range(25):
+    new_routes = []
+    for route_counter in robot_routes:
+        new_route = Counter()
+        for sub_route, qty in route_counter.items():
+            new_counts = routes2(sub_route, dir_pad)
+            for k in new_counts:
+                new_counts[k] *= qty
+            new_route.update(new_counts)
+        new_routes.append(new_route)
+    robot_routes = new_routes
+
+p2 = sum(route_len(route)*int(line[:-1])
+         for route, line in zip(robot_routes, lines))
+print("Part 2:", p2)
