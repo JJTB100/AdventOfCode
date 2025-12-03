7318237ea24a939ffcb681ba3a9633bb7659c8aa
Jonathan Broster <jonathan@broster.co.uk>
2025-12-03 10:56:40 +0000

    Update day3.py

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -1,14 +1,14 @@
 def get_largest_n(input, n, low, high):
-    if(n<1): return ''
+    if(n<=1): return ''
     string = input[low:high] if high != 0 else input[low:]
     largest = ''
     index = 0
     for i,c in enumerate(string):
-        if c >= largest:
+        if c > largest:
             largest = c
             index = i
     
-    return largest+str(get_largest_n(input, n-1, index+1+low, -(n-1)))
+    return largest+str(get_largest_n(input, n-1, index+1+low, -(n-3)))
     
 
 p1 = 0
@@ -17,8 +17,7 @@ with open("input.in") as f:
         output = ""
         line = line.strip()
     
-        output += get_largest_n(line, 12, 0, -1)
+        output += get_largest_n(line, 12, 0, -11)
         print(output)
-        for a in output:
-            p1 += int(a)
+        p1 += int(output)
 print(p1)
