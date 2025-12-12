591b448a5a6f63e856d43010f4d276dfd6d21013
Jonathan Broster <jonathan@broster.co.uk>
2025-12-12 14:34:42 +0000

    day11

---
+++ 2025/day12/day12.py
@@ -0,0 +1,21 @@
+# Algorithm X
+# Matrix of moves in the rows and constraints in the columns
+# Primary Cols: Shapes
+# Secondary Cols: Coords
+# https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
+# Must Pre-calculate rotations and reflections
+# Each row: "Shape X, Instance Y, placed at (r, c) with Rotation R."
+# 
+
+## Pre-Processing the Rotations and Flipping
+# Take a shape and generate all 8 symmetries
+# Deduplicate i.e. remove symmetries that are the same
+# Generate Rows on the matrix by shifting the unique shapes to every valid (x,y) on the grid
+
+## Algorithm X (Dancing Links)
+# Pick a Primary Col <with the fewest 1s> (fewest valid moves for that piece)
+# Select a row that covers this column
+#   Remove the primary col (constraints met)
+#   Remove all secondary cols (cells) occupied by that row
+#   Remove all other rows that needed any of those grid cells
+#   Remove all other rows that used the same piece
