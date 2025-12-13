10b706313c47ed5b9f733bf9e1b69ddf9bb119eb
Jonathan Broster <jonathan@broster.co.uk>
2025-12-13 17:29:12 +0000

    Update day12.py

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -19,3 +19,14 @@
 #   Remove all secondary cols (cells) occupied by that row
 #   Remove all other rows that needed any of those grid cells
 #   Remove all other rows that used the same piece
+
+with open("input.in") as f:
+    lines = f.readlines()
+
+p1 = 0
+for line in lines:
+    if len(line) > 5:
+        if (int(line[0:2]) * int(line[3:5])) >= (9 * sum(int(num) for num in line.split(": ")[1].split(" "))):
+            p1 += 1
+            
+print(p1)
